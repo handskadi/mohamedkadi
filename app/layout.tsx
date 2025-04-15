10ff521eab9bef39d58deef4f6dce45be2c761f2
Mohamed KADI <handskadi@gmail.com>
2025-04-15 16:57:51 +0100

    Remove consent mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,35 +56,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <head>
-        {/* ✅ Google Tag Manager Script (in <head>) */}
-        {/* <Script id="gtm-init" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-54SPGC3S');
-          `}
-        </Script> */}
-      </head>
       <body className="dark:bg-gray-900 transition-colors">
-        {/* ✅ Google Tag Manager (noscript) for <body> fallback */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-54SPGC3S"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          ></iframe>
-        </noscript>
-
-
         <Navbar />
         <main className="container mx-auto mt-8">{children}</main>
         <Footer />
         {/* ✅ Schema.org JSON-LD for Full Stack Developer */}
-        {/* <Script
+        <Script
           id="structured-data"
           type="application/ld+json"
           strategy="afterInteractive"
@@ -118,7 +95,7 @@ export default function RootLayout({
               ]
             }),
           }}
-        /> */}
+        />
       </body>
     </html>
   );
